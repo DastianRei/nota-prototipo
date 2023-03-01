b07d89337478be2e715de2f1422339c345f0bbbe
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-01 14:33:18 -0400

    Se creo el formulario de crear notas

--- src/presenter-notas.js
+++ src/presenter-notas.js
@@ -3,7 +3,14 @@ import Nota from "./Nota";
 const tituloABuscar = document.querySelector("#titulo-a-buscar");
 const divTituloABuscar = document.querySelector("#titulo");
 const formBuscador = document.querySelector("#buscador");
+const formCrear = document.querySelector("#crear-notas");
+
+
 formBuscador.addEventListener("submit", (event) => {
   event.preventDefault();
   divTituloABuscar.innerHTML = "<p>" + tituloABuscar.value + "</p>";
+});
+formCrear.addEventListener("submit", (event) => {
+    event.preventDefault();
+
 });
